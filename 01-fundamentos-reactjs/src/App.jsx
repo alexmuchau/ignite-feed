33a88b0141d20679216db7948c12115eae0dc077
Alex Muchau <srrplays@gmail.com>
2022-08-12 10:17:52 -0300

    header-done

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,14 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post"
+
+import "./global.css";
 
 function App() {
   return (
-    <Post/>
+    <>
+      <Header/>
+      <Post/>
+    </>
   )
 }
 
