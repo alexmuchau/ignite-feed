245afd5a3d9627c0c56601da0fa98a6564a838fb
Alex Muchau <srrplays@gmail.com>
2022-08-15 17:13:45 -0300

    post-component

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,41 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/alexmuchau.png" alt="" />
+          <div className={styles.authorInfo}>
+            <strong>Alex Muchau</strong>
+            <span>Web Dev</span>
+          </div>
+        </div>
+
+        <time title="15 de agosto ás 08:13h" dateTime="2022-05-11 08:13:00">Publica ha 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉{" "}<a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{" "}
+          <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  )
+}
