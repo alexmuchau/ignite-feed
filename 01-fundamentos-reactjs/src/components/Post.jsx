c357a33819c153155bea20b836b2795e426c9bb5
Alex Muchau <srrplays@gmail.com>
2022-08-16 10:24:17 -0300

    design-done

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -7,7 +8,7 @@ export function Post() {
       <header>
 
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/alexmuchau.png" alt="" />
+          <Avatar src="https://github.com/alexmuchau.png"/>
           <div className={styles.authorInfo}>
             <strong>Alex Muchau</strong>
             <span>Web Dev</span>
